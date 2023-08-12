c064e79ac9debbdc6da8d23e1836658d1ad778a1
kaifi ahmed <kaifiahmed12k@gmail.com>
2023-08-12 15:05:01 +0530

    this is my quiz app

---
+++ app/src/main/java/com/example/quiztopper/QuesionAnswerHain.java
@@ -0,0 +1,25 @@
+package com.example.quiztopper;
+
+public class QuesionAnswerHain {
+
+    public static String question [] = {
+            "which company owns the android?",
+            "which one is not the programming language ?",
+            "where you are watching this video ?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple" , "Nokia" , "Samsung"},
+            {"java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","YouTube"}
+
+    };
+
+    public static String correctAnswer[] = {
+            "Google",
+            "Notepad",
+            "YouTube"
+
+    };
+}
